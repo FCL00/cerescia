d35eed9bf2b884d2dc5f5ed3ee9c9f7ad24fb747
FCL00 <flagahit00@gmail.com>
2023-12-14 13:33:10 +0800

    add login and sign up feature

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,16 +2,46 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+use App\Http\Requests\UserRequest;
+use Illuminate\Validation\Rules\Password;
 
 class UserController extends Controller
 {
+    public function logout() {
+        auth()->logout();
+        return redirect('/')->with("success", "You have successfully logout");
+    }
+    public function login(Request $request){
+        $incomingData = $request->validate([
+            "username" => "required",
+            "password" => "required",
+        ]);
+        if(auth()->attempt(['username' => $incomingData["username"], 'password' => $incomingData["password"]  ])){
+            $request->session()->regenerate();
+            return redirect('/store')->with("success", "You have successfully login");
+        } else {
+            return redirect('/login')->with("failure", "Incorrect username or password");
+        }
+    }
+    public function signUp(UserRequest $request){
+        $incomingData = $request->validated();
+
+        $user = User::create($incomingData);
+        auth()->login($user);
+
+        return view("storePage");
+    }
+    public function showSignUp(){
+        return view("signUpPage");
+    }
     public function showLoginPage(){
         return view("loginPage");
     }
     public function showHomePage(){
         return view("homePage");
     }
-
 
 }
