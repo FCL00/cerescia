2e0cc0adfaf5a47a4ac881ece8d448051bcfac3f
FCL00 <flagahit00@gmail.com>
2023-12-16 09:27:56 +0800

    add checkout, cart transaction

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -6,8 +6,15 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    //
-
+    public function showCheckOutForm(){
+        return view("checkoutPage");
+    }
+    public function showTransaction(){
+        return view("transaction");
+    }
+    public function showCart(){
+        return view("cartPage");
+    }
     public function showStore(){
         return view("storePage");
     }
